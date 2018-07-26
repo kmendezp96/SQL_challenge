2f0aead2aacb569ae104960a2dab13d1c7ba2a12
Kevin Mendez <kmendezp@unal.edu.co>
2018-07-26 18:22:10 -0500

    trigger challenge update

---
+++ triggers.sql
@@ -0,0 +1,25 @@
+drop trigger if exists trigger_challenge;
+DELIMITER $$
+CREATE TRIGGER trigger_challenge BEFORE UPDATE ON challenge
+	FOR EACH ROW BEGIN
+		declare new_trgr int;
+        declare old_trgr int;
+        declare seqUpdt int;
+		SET new_trgr = NEW.trgr;
+		SET old_trgr = OLD.trgr;
+		SET seqUpdt = OLD.seq;
+		if new_trgr <>old_trgr THEN
+			if new_trgr%2 = 0 THEN
+				SET NEW.trgr = new_trgr + 3;
+			else
+				SET New.trgr = new_trgr - 5;
+            
+            END IF;
+		END IF;
+	END;
+	$$
+DELIMITER ;
+
+
+-- 
+
