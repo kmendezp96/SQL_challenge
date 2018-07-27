182f038c51e7ba6758f3229812cbf7494cd0467c
Kevin Ruiz <kevin.ruiz@endava.com>
2018-07-27 17:54:27 -0500

    stored procedure (min and max, mode, media)

--- calculus.sql
+++ calculus.sql
@@ -4,12 +4,15 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS calculus;
 CREATE PROCEDURE `calculus`()
 begin 
+
+declare total_rows int default 0;
+set total_rows = count(*)/2;
+
 select ccnumber, name from challenge where ccnumber = ( select min(ccnumber) from challenge);
 select ccnumber, name from challenge where ccnumber = ( select max(ccnumber) from challenge);
-SELECT ccnumber, COUNT( * )
-FROM challenge
-GROUP BY ccnumber
-ORDER BY COUNT( * ) DESC
-LIMIT 1;
+SELECT ccnumber, COUNT( * ) FROM challenge GROUP BY ccnumber ORDER BY COUNT( * ) DESC LIMIT 1;
+select avg(ccnumber) from challenge;
+
+select ccnumber from challenge  order by ccnumber asc limit total_rows,1;
 
 end
